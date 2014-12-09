55b9107421f17510f52223a84bb749c05222397f
Nisamitra <ya0898770087>
2014-12-09 17:33:45 +0700

    add new web6

---
+++ datatable.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="903" border="1">
+  <tr>
+    <td width="800"><table width="800" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><img src="dwdb/623212-img-12.jpg" width="800" height="185" /></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+    </table></td>
+    <td width="87" colspan="4">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td colspan="4">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="5">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
